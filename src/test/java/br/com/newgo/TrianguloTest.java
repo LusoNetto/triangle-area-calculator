110c36082803d647bcb76b0a238fd4133e762bfa
Luciano Junior <lusonetto137@outlook.com>
2024-07-31 14:30:22 -0300

    feat: Escrever um teste para o método de cálculo da área usando base e altura.

--- src/test/java/br/com/newgo/TrianguloTest.java
+++ src/test/java/br/com/newgo/TrianguloTest.java
@@ -2,11 +2,13 @@ package br.com.newgo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+
 
 public class TrianguloTest {
     Triangulo triangulo;
@@ -18,7 +20,20 @@ public class TrianguloTest {
 
     @Test
     @DisplayName("When instance triangulo")
-    void triangleTest(){
+    void testInstance(){
         assertNotNull(triangulo, "triangulo is null");
     }
+
+    @Nested
+    @DisplayName("Testes de Cálculo de Área usando Base e Altura")
+    class BaseHeightTests {
+        @Test
+        @DisplayName("When calc a area using base and height")
+        void calculateBaseAndHeight(){            
+            triangulo.setBase(5);
+            triangulo.setAltura(10);
+            assertEquals(25, triangulo.calcularAreaBaseAltura(), "Escrever um teste para o método de cálculo da área usando base e altura.");
+        }
+    }
+    
 }
