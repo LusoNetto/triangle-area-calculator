1fdcc9d54b1f8a3bde41c35d008ab965bc8bf852
Luciano Junior <lusonetto137@outlook.com>
2024-07-31 13:13:22 -0300

    feat: Implementar métodos para set/get dos atributos.

--- src/main/java/br/com/newgo/Triangulo.java
+++ src/main/java/br/com/newgo/Triangulo.java
@@ -15,6 +15,46 @@ public class Triangulo {
         this.base = base;
     }
 
+    public double getLadoA() {
+        return ladoA;
+    }
+
+    public void setLadoA(double ladoA) {
+        this.ladoA = ladoA;
+    }
+
+    public double getLadoB() {
+        return ladoB;
+    }
+
+    public void setLadoB(double ladoB) {
+        this.ladoB = ladoB;
+    }
+
+    public double getLadoC() {
+        return ladoC;
+    }
+
+    public void setLadoC(double ladoC) {
+        this.ladoC = ladoC;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
     
     
     
