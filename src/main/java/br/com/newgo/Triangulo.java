0845b62c28b2d150c56e8ad1c1ede70b36b5918c
Luciano Junior <lusonetto137@outlook.com>
2024-07-31 13:12:29 -0300

    feat: Implementar a classe Triangle para passar o teste.

--- src/main/java/br/com/newgo/Triangulo.java
+++ src/main/java/br/com/newgo/Triangulo.java
@@ -1,5 +1,21 @@
 package br.com.newgo;
 
 public class Triangulo {
+    private double ladoA;
+    private double ladoB;
+    private double ladoC;
+    private double altura;
+    private double base;
+
+    public Triangulo(double ladoA, double ladoB, double ladoC, double altura, double base) {
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
+        this.ladoC = ladoC;
+        this.altura = altura;
+        this.base = base;
+    }
+
+    
+    
     
 }
